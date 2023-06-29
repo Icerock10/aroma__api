f5bef53fa9add3641f35f5a3b11e013e4884d25d
Icerock10 <dp021190dms@gmail.com>
2023-06-29 19:13:58 +0300

    added prod command

--- config/db.ts
+++ config/db.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 export const connectDb = async (): Promise<void> => {
-  const MONGO_URI: string = process.env.MONGO_URI;
+  const MONGO_URI: string =
+    'mongodb+srv://icerock10:markeloff123@aroma.6hmchiw.mongodb.net/?retryWrites=true&w=majority';
   try {
     await mongoose.connect(MONGO_URI);
     console.log('mongoDB connected');
